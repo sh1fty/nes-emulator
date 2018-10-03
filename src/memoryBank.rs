6061bc0642ec7a4dea865867fc04a408bd0da44d
Veroon Budhai <veroon@minatjanster.se>
2018-10-03 22:08:43 +0200

    test commit

---
+++ src/memoryBank.rs
@@ -0,0 +1,15 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    /// Add two signed integers.
+    return a + b;
+}
+
+#[cfg(test)]
+mod test {
+
+    use memoryBank;
+
+    #[test]
+    fn memoryBankTest() {
+        assert_eq!(memoryBank::add(1, 1), 2);
+    }
+}
